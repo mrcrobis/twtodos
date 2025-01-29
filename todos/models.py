17a4e058aca806a83e41f02fa0019d032e6a4456
Joao Vitor <joaovrsilveira16@gmail.com>
2025-01-29 17:24:03 -0300

    Configurações de formatação

--- todos/models.py
+++ todos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Todo(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     created_add = models.DateTimeField(auto_now_add=True, null=False, blank=False)

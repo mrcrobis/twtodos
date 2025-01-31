822f16dee2e0df6aec8c12b3c0ea1c64a5f919fc
Joao Vitor <joaovrsilveira16@gmail.com>
2025-01-31 17:29:02 -0300

    Criação de página de edição de tarefas

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from todos.views import TodoListView, TodoCreateView
+from todos.views import TodoListView, TodoCreateView, TodoUpdateView
+
+# pk = primary key (parecido com id)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TodoListView.as_view(), name="todo_list"),
     path("create/", TodoCreateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,18 +1,23 @@
 # from django.shortcuts import render
 from .models import Todo
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 
 # def todo_list(request):
 #    todos = Todo.objects.all()
 #    return render(request, "todos/todo_list.html", {"todos": todos})
 
-
+# a funcao acima faz a mesma coisa que essa classe 
 class TodoListView(ListView):
     model = Todo
 
 
 class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
